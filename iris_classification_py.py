5c0e5b76b0c322d0effe5b603e4b6ca0136257b3
laharipothumudi <149651387+laharipothumudi@users.noreply.github.com>
2025-05-15 13:32:02 +0530

    Update iris_classification_py.py

--- iris_classification_py.py
+++ iris_classification_py.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMOD0isVLcv8dpRYXyiFAuA",
-      "include_colab_link": false
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
